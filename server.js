b3735c01fa6c00824c72b08af600fd7c3a2dce23
monjemaica <201810138@gordoncollege.edu.ph>
2021-12-19 10:32:10 +0800

    implement charoom socketio

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const path = require('path');
+const moment = require('moment');
+const {userJoin,getRoomUsers,userLeave} = require('./src/utils/messages')
 // const auth = require('./src/services/setUser');
 // const { authUser } = require('./src/services/auth');
 
-const app = express();
 const PORT = 5000 || process.env.PORT;
+
+const http = require('http');
+const server = http.Server(app);
+
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "*"
+    }
+  });
 
 // Middlewares
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/uploads', express.static('uploads'));
+app.use(express.static(path.join(__dirname, 'src')));
 // app.use(auth.setUser);
 
 // app.get('/admin', authUser, (req, res) => {
@@ -25,7 +39,48 @@ var corsOptions = {
 
 require('./src/routes/student_routes.js')(app);
 
-app.listen(PORT, (err, res) => {
+io.on('connection', (socket) => {
+    console.log('New WS Connection...');
+
+    socket.on('join', (data) => {
+        const users = userJoin(socket.id, data.user, data.room);
+
+        console.log('users-arr: ', users)
+        socket.join(data.room);
+        console.log(`${data.user} joined the room : ${data.room}`);
+        socket.broadcast.to(data.room).emit('new-user-joined', {user:data.user, message:'has joined this room'});
+
+        io.to(data.room).emit('room-users', {room: data.room, users: getRoomUsers(data.room)})
+    })
+
+    socket.on('leave', (data)=>{
+        const users = userLeave(socket.id);
+        console.log('users-arr: ', users)
+        console.log(`${data.user} left the room : ${data.room}`);
+        socket.broadcast.to(data.room).emit('left-room', {user:data.user, message:'has left this room'});
+        socket.leave(data.room);
+        io.to(data.room).emit('room-users', {room: data.room, users: getRoomUsers(data.room)});
+    })
+
+    socket.on('message', (data) => {
+        io.in(data.room).emit('new-message', {user:data.user, message: data.message, date: moment().format()});
+    } )
+
+    // socket.emit('message', formatMessage('chatroom', 'Welcome to ChatRoom'));
+    
+    // socket.broadcast.emit('message', formatMessage('chatroom', 'A user has joined the chat'));
+
+    // socket.on('disconnect', () => {
+    //     io.emit('message', 'chatroom', 'A user has left the chat')
+    // }); 
+
+    // socket.on('chat-message', (message) => {
+    //   io.emit('message', formatMessage('USER', message));
+    //   console.log(message);
+    // });
+});
+
+server.listen(PORT, (err, res) => { 
     if(err){
         throw(err);
     }
